c5a5259e0eee903c674166b788cb26550a4d562a
runarorama <runarorama@gmail.com>
2012-01-20 21:22:09 -0500

    A semi-naive bug

--- src/REPL.hs
+++ src/REPL.hs
@@ -6,6 +6,14 @@ import Katalog
 import Control.Monad.Trans
 import Control.Monad.State
 import Data.Monoid
+import Control.Applicative
+
+commands :: [(String, StateT Datalog (InputT IO) ())]
+commands = [("facts", ac fst),
+            ("rules", ac snd),
+            ("edb", ac $ uncurry naive)]
+
+ac f = get >>= lift . outputStrLn . show . doc . f
 
 repl :: IO ()
 repl = runInputT defaultSettings $ evalStateT loop ([],[])
@@ -15,7 +23,9 @@ repl = runInputT defaultSettings $ evalStateT loop ([],[])
      case minput of
        Nothing -> return ()
        Just ":q" -> return ()
+       Just (':' : c) -> (maybe (unrecognized c) id $ lookup c commands) >> loop
        Just input -> do
          modify . either (error . show) (flip mappend) $ run input
          loop
+   unrecognized c = lift $ outputStrLn ("Unrecognized command " ++ c)
 
